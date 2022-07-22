09e0383c9f548a0a6c989238b29c3def22b831b5
Lucas Payton <lucasjpayton101@gmail.com>
2022-07-22 00:01:14 -0500

    Further Basic Setup

--- E-Commerce/server/controllers/ProductController.js
+++ E-Commerce/server/controllers/ProductController.js
@@ -4,10 +4,12 @@ import BaseController from "../utils/BaseController";
 
 export class ProductController extends BaseController {
   constructor() {
-    super('product')
+    super('products')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getAllProducts)
+      .get('/:productId', this.getProductById)
+      .get('/:productId/reviews', this.getReviewsByProductId)
   }
 
   async getAllProducts(req, res, next) {
@@ -15,6 +17,24 @@ export class ProductController extends BaseController {
       const query = req.body
       const products = await productService.getAllProducts(query)
       return res.send(products)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getProductById(req, res, next) {
+    try {
+      const product = await productService.getProductById(req.params.productId);
+      return res.send(product)
+    } catch (error) {
+      next (error)
+    }
+  }
+
+  async getReviewsByProductId(req, res, next) {
+    try {
+      const reviews = await
+      return res.send(reviews)
     } catch (error) {
       next(error)
     }
