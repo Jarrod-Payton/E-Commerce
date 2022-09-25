4d78afd42f05e7649695f3ee9423d2f492e158d1
Lucas Payton <lucasjpayton101@gmail.com>
2022-09-24 22:13:46 -0500

    Basic Setup. (just a few tweeks)

--- E-Commerce.client/src/pages/AboutPage.vue
+++ E-Commerce.client/src/pages/AboutPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
-  </div>
+<div class="about-page px-5">
+  <h1>This is an E-Commerce site that is a display piece for <a href="https://www.linkedin.com/in/lucaspayton/" target="_blank">Lucas Payton</a> to demonstrate his beautiful and gracefull abilities as developer</h1>
+</div>
 </template>
 
 <script>
