72714a67bf30e1c390b2149ef4c9c20101781540
Lucas Payton <lucasjpayton101@gmail.com>
2022-10-04 22:29:17 -0500

    tiny tiny changes

--- E-Commerce.client/src/components/CreateModal.vue
+++ E-Commerce.client/src/components/CreateModal.vue
@@ -2,33 +2,31 @@
   <div class="modal fade" id="createModal">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Create a Posting</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-          ></button>
-        </div>
-        <div class="modal-body">
-          <input type="text" v-model="form.name" />
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Cancel
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary text-white"
-            @click="create()"
-          >
-            Create
-          </button>
-        </div>
+        <form @submit.prevent="create()">
+          <div class="modal-header">
+            <h5 class="modal-title">Create a Posting</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <input type="text" v-model="form.name" />
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button type="submit" class="btn btn-primary text-white">
+              Create
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -46,6 +44,7 @@ export default {
       async create() {
         try {
           console.log(form.value.name);
+          form.value.name = "";
         } catch (error) {
           Pop.toast(error);
         }
